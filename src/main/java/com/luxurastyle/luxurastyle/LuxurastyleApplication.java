3b6d8950e00edd0dfc7be2916d1992ff67fb3293
Dhruv Desai <ddesai7@wisc.edu>
2024-10-06 12:54:42 -0500

    Initial Commit

---
+++ src/main/java/com/luxurastyle/luxurastyle/LuxurastyleApplication.java
@@ -0,0 +1,13 @@
+package com.luxurastyle.luxurastyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxurastyleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LuxurastyleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/luxurastyle/luxurastyle/LuxurastyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxurastyle.luxurastyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxurastyleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
