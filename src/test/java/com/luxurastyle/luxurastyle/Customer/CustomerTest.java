8560fcd58151a384c87895b27d9fb4edf8649967
RSYashwanth <yashwanthrs1104@gmail.com>
2024-10-09 13:21:04 -0500

    Added basic test

--- src/test/java/com/luxurastyle/luxurastyle/Customer/CustomerTest.java
+++ src/test/java/com/luxurastyle/luxurastyle/Customer/CustomerTest.java
@@ -1,4 +1,25 @@
 package com.luxurastyle.luxurastyle.Customer;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class CustomerTest {
+  @Test
+  public void testCustomerGettersSetters() {
+    Customer customer = new Customer();
+
+    customer.setId(10000001l);
+    customer.setName("Test");
+    customer.setEmail("test@test.com");
+    customer.setPassword("Test123");
+    customer.setAdmin(true);
+
+    Assertions.assertEquals(10000001l, customer.getId());
+    Assertions.assertEquals("Test", customer.getName());
+    Assertions.assertEquals("test@test.com", customer.getEmail());
+    Assertions.assertEquals("Test123", customer.getPassword());
+    Assertions.assertTrue(customer.getAdmin());
+  }
 }
